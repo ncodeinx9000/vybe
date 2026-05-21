e2756e98f007bbd4deeaf980b5b8d05df9b8d24f
Nitin Kumar Singh <138771147+ncodeinx9000@users.noreply.github.com>
2026-05-21 14:29:41 +0530

    Update server URL to production endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,7 +28,7 @@ import getAllNotifications from "./hooks/getAllNotifications";
 import Notifications from "./pages/Notifications";
 import { setNotificationData } from "./redux/userSlice";
 
-export const serverUrl = "http://localhost:8000";
+export const serverUrl = "https://vybe-backend-1m8s.onrender.com";
 function App() {
   getCurrentUser();
   getSuggestedUser();
